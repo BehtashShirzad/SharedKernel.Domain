50c116b4244d28d22e200ea59beb2acb56ab2dbf
Mohamad <80650043+MDshirzad@users.noreply.github.com>
2025-06-12 21:51:19 +0400

    renaming

--- SharedKernel.Domain/SharedKernel.Domain/BaseEntities/Contracts/IRepository.cs
+++ SharedKernel.Domain/SharedKernel.Domain/BaseEntities/Contracts/IRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SharedKernel.Domain.Interfaces
+namespace SharedKernel.Domain.BaseEntities.Contracts
 {
     public interface IRepository
     {
